12d2c460885029c4a5b757639320983460aeac81
gumonteilh <guillaume.monteilhet@etu.uca.fr>
2022-03-08 00:37:02 +0100

    delete transfert + add revoke role

--- contracts/Guigui.sol
+++ contracts/Guigui.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -8,7 +8,7 @@ contract AccessControl {
     event RevokeRole(bytes32 indexed role, address indexed account);
     mapping(bytes32 => mapping(address => bool)) public roles;
 
-    bool private allowAutoGrantRole = true;
+    bool public allowAutoGrantRole = true;
 
     bytes32 internal constant OWNER = keccak256(abi.encodePacked("OWNER"));
     bytes32 internal constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
@@ -21,6 +21,7 @@ contract AccessControl {
 
     constructor() {
         _grantRole(OWNER, msg.sender);
+        _grantRole(ADMIN, msg.sender);
     }
 
     function _grantRole(bytes32 _role, address _account) internal {
@@ -35,6 +36,18 @@ contract AccessControl {
         _grantRole(_role, _account);
     }
 
+    function _revokeRole(bytes32 _role, address _account) internal {
+        roles[_role][_account] = false;
+        emit RevokeRole(_role, _account);
+    }
+
+    function revokeRole(bytes32 _role, address _account)
+        external
+        onlyRole(OWNER)
+    {
+        _revokeRole(_role, _account);
+    }
+
     // this function is not supposed to exist but I created it to allow to test the application
     function autoGrantAdminRole() external {
         require(
@@ -45,11 +58,11 @@ contract AccessControl {
         _grantRole(ADMIN, msg.sender);
     }
 
-    function setAllowAutoGrantRole(bool _allowAutoGrantRole)
+    function inverseAllowAutoGrantRole()
         external
         onlyRole(OWNER)
     {
-        allowAutoGrantRole = _allowAutoGrantRole;
+        allowAutoGrantRole = !allowAutoGrantRole;
     }
 }
 
@@ -57,9 +70,7 @@ contract Guigui is ERC20, AccessControl {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _grantRole(OWNER, msg.sender);
-        _grantRole(ADMIN, msg.sender);
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) AccessControl()  {
     }
 
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Guigui = artifacts.require("Guigui");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Guigui, "guigui", "GG");
 };
